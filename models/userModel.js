33bd1431ea4303b41c5e7a8364124cedab6bc661
macas5 <mkalinauskas1993@gmail.com>
2022-09-23 14:10:39 +0300

    remove required from name and email

--- models/userModel.js
+++ models/userModel.js
@@ -13,11 +13,9 @@ const userSchema = new mongoose.Schema(
     },
     name: {
       type: String,
-      required: true,
     },
     email: {
       type: String,
-      required: true,
     },
     isAdmin: {
       type: Boolean,
